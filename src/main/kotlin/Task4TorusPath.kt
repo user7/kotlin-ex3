3be96519bfd6abfcae5ec1f231f08bd550b85e28
svr <svr@protonmail.com>
2022-05-30 16:50:23 +0300

    file based test

--- src/main/kotlin/Task4TorusPath.kt
+++ src/main/kotlin/Task4TorusPath.kt
@@ -1,77 +1,192 @@
 import java.io.File
 import java.util.*
 
+var inputFile = "input.txt"
+var outputFile = "output.txt"
 fun main(args: Array<String>) {
-    doTorus()
+    //doTorus()
+    testToruses()
 }
 
-data class XY(val x: Int, val y: Int)
-data class Dir(val id: Int, val revId: Int, val dx: Int, val dy: Int, val code: Char)
+data class Pt(val x: Int, val y: Int)
 
-val dirs = arrayOf(
-    Dir(1, 3, 0, 1, 'N'),
-    Dir(2, 1, 0, 'E'),
-    Dir(3, 0, -1, 'S'),
-    Dir(4, -1, 0, 'W'),
-)
-val cellBlocked = -1
+class Cell {
+    var prev: Cell? = null
+    var code: Char = ' '
+}
 
 class Torus(
-    val size: XY,
-    val start: XY,
-    val finish: XY
+    val size: Pt,
+    val start: Pt,
+    val finish: Pt
 ) {
-    val data = IntArray(size.x * size.y)
-    fun setAt(at: XY, value: Int) = value.let { data[offset(at)] = it }
-    fun getAt(at: XY): Int = data[offset(at)]
-    fun step(at: XY, dir: Dir) = XY(wrap(at.x + dir.dx, size.x), wrap(at.y + dir.dy, size.y))
-
-    fun offset(at: XY) = at.x * size.y + at.y
-    fun wrap(pos: Int, maxPos: Int) = (pos + maxPos) % maxPos
+    private val data = Array(size.x * size.y) { Cell() }
+    fun at(at: Pt) = data[at.x * size.y + at.y]
 }
 
 fun doTorus() {
-    val s = Scanner(File("input.txt"))
-    fun readXY(): XY {
-        val y = s.nextInt()
-        val x = s.nextInt()
-        return XY(x, y)
-    }
-
-    val size = readXY()
-    val t = Torus(size, readXY(), readXY())
-    for (x in 0 until size.x)
-        for (y in 0 until size.y)
-            if (s.nextInt() != 0)
-                t.setAt(XY(x, y), cellBlocked)
-
-    val out = findPath(t)
-    File("output.txt").writeText(out ?: "-1")
+    val s = Scanner(File(inputFile))
+    fun readPt() = s.nextInt().let { Pt(s.nextInt(), it) }
+    val t = Torus(readPt(), readPt(), readPt())
+    for (y in 0 until t.size.y)
+        for (x in 0 until t.size.x)
+            if (s.nextInt() == 1)
+                t.at(Pt(x, y)).code = 'x'
+    File(outputFile).writeText(findPath(t))
 }
 
-fun findPath(t: Torus): String? {
-    val wave = LinkedList<XY>()
-    wave.add(t.start)
-    val wave2 = LinkedList<XY>()
-    while(true) {
-        for (pt in wave) {
-            for (dir in dirs) {
-                var step = t.step(pt, dir)
-                if (t.getAt(step) == 0) {
-                    t.setAt(step, dir.id)
-                    if (step == t.finish) {
-                        var path = String()
-                        while (step != t.start) {
-                            val fDir = t.getAt(step)
-                            path += dirs[fDir - 1]
-                            step = t.step(step, dirs[(fDir - 1 + 2) % 2 + 1])
-                        }
+fun findPath(t: Torus): String {
+    if (t.start == t.finish)
+        return ""
+    t.at(t.start).code = 'x'
+    t.at(t.finish).code = ' '
+    var wave = listOf(t.start)
 
+    // step directions
+    val dirCode = arrayOf('N', 'E', 'S', 'W')
+    val dx = arrayOf(0, 1, 0, -1)
+    val dy = arrayOf(1, 0, -1, 0)
+
+    while (wave.isNotEmpty()) {
+        val wave2 = LinkedList<Pt>()
+        for (pt in wave) {
+            val cell = t.at(pt)
+            for (dir in dirCode.indices) {
+                val pt2 = Pt((pt.x + dx[dir] + t.size.x) % t.size.x, (pt.y + dy[dir] + t.size.y) % t.size.y)
+                val cell2 = t.at(pt2)
+                if (cell2.code != ' ')
+                    continue
+                cell2.code = dirCode[dir]
+                cell2.prev = cell
+                if (pt2 == t.finish) {
+                    var cur = cell2
+                    var path = ""
+                    while (true) {
+                        val next = cur.prev ?: return path
+                        path = cur.code + path
+                        cur = next
                     }
-                    wave2.add(step)
                 }
+                wave2.add(pt2)
             }
         }
-        if ()
+        wave = wave2
     }
+    return "-1"
+}
+
+fun String.splitIntsChunk(n: Int) = this.split(" ").filter { it != "" }.map { it.toInt() }.chunked(n)
+fun String.toPoints() = this.splitIntsChunk(2).map { Pt(it[0], it[1]) }
+
+fun testInOut(name: String, input: String, expected: String) {
+    inputFile = "in.txt"
+    outputFile = "out.txt"
+    val exp = expected.trimMargin()
+    File(inputFile).writeText(input.trimMargin())
+    doTorus()
+    val got = File(outputFile).readText()
+    if (exp != got) {
+        val flatExp = exp.replace("\n", " ")
+        val flatGot = got.replace("\n", " ")
+        println("failed $name: expected '$flatExp', got '$flatGot'")
+    } else {
+        println("ok $name")
+    }
+}
+
+fun testToruses() {
+
+    testInOut(
+        name = "3x3 neighbor",
+        input = """
+            |3 3 0 0 1 0
+            |0 0 0
+            |0 0 0
+            |0 0 0
+        """,
+        expected = "N",
+    )
+
+    testInOut(
+        name = "3x3 neighbor",
+        input = """
+            |3 3 0 0 2 0
+            |0 0 0
+            |0 0 0
+            |0 0 0
+        """,
+        expected = "S",
+    )
+
+    testInOut(
+        name = "3x3 neighbor",
+        input = """
+            |3 3 0 0 0 1
+            |0 0 0
+            |0 0 0
+            |0 0 0
+        """,
+        expected = "E",
+    )
+
+    testInOut(
+        name = "3x3 neighbor",
+        input = """
+            |3 3 0 0 0 2
+            |0 0 0
+            |0 0 0
+            |0 0 0
+        """,
+        expected = "W",
+    )
+
+    testInOut(
+        name = "3x3 angle path",
+        input = """
+            |3 3 0 0 1 1
+            |0 0 0
+            |0 0 0
+            |0 0 0
+        """,
+        expected = "NE",
+    )
+
+    testInOut(
+        name = "3x3 wrap angle path",
+        input = """
+            |3 3 0 0 2 1
+            |0 0 0
+            |0 0 0
+            |0 0 0
+        """,
+        expected = "ES",
+    )
+
+    testInOut(
+        name = "5x1 direct path",
+        input = """
+            |1 5 0 0 0 2
+            |0 0 0 0 0
+        """,
+        expected = "EE",
+    )
+
+    testInOut(
+        name = "5x1 blockage wrapped path",
+        input = """
+            |1 5 0 0 0 2
+            |0 1 0 0 0
+        """,
+        expected = "WWW",
+    )
+
+    testInOut(
+        name = "7x2 snake path",
+        input = """
+            |2 7 0 0 1 5
+            |0 1 0 0 0 1 0
+            |0 0 0 1 0 0 1
+        """,
+        expected = "NEENEENE",
+    )
 }
